6e472b5dd48f3c91a9620504df2a2ea76917082b
dhavalsharma97 <dhaval.sharma97@gmail.com>
2021-01-24 15:48:44 -0800

    Base Page Added

--- house_price/buyers_offer/admin.py
+++ house_price/buyers_offer/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Payment, PropertyAddress, Buyer
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payment_type', 'down_payment')
+    list_filter = ['payment_type']
+
+@admin.register(PropertyAddress)
+class PropertyAddressAdmin(admin.ModelAdmin):
+    list_display = ('apartment', 'street', 'city', 'state', 'country')
+    list_filter = ['city', 'state']
+    fields = [('apartment', 'street'), ('city', 'state'), 'country']
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'offer_price')
+    fieldsets = (
+        ('Personal Details', {
+            'fields': ('first_name', 'last_name', 'email', 'phone')
+        }),
+        ('Property Details', {
+            'fields': ('offer_price', 'property_address', 'payment')
+        })
+    )
--- house_price/buyers_offer/models.py
+++ house_price/buyers_offer/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+
+class Payment(models.Model):
+    """Model representing a payment type"""
+    PAYMENT_TYPES = (
+        ('Cash', 'Cash'),
+        ('Loan', 'Loan')
+    )
+
+    payment_type = models.CharField(max_length=50, choices=PAYMENT_TYPES, help_text="Enter a type of payment")
+    down_payment = models.IntegerField(help_text="Enter the amount of down payment for the loan")
+
+    def __str__(self):
+        """String for representing the model object"""
+        return f'{self.payment_type}, {self.down_payment}'
+
+
+class PropertyAddress(models.Model):
+    """Model representing a payment type"""
+    apartment = models.CharField(max_length=50, help_text="Enter the property apartment")
+    street = models.CharField(max_length=50, help_text="Enter the property street")
+    city = models.CharField(max_length=50, help_text="Enter a type of property city")
+    state = models.CharField(max_length=50, help_text="Enter a type of property state")
+    country = models.CharField(max_length=50, help_text="Enter a type of property country")
+    pincode = models.IntegerField(help_text="Enter a type of property pincode")
+
+    def __str__(self):
+        """String for representing the model object"""
+        return self.apartment
+
+
+class Buyer(models.Model):
+    """Model representing a Buyer"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular buyer')
+    first_name = models.CharField(max_length=100, help_text="Enter the first name")
+    last_name = models.CharField(max_length=100, help_text="Enter the last name")
+    email = models.EmailField(help_text="Enter the email address")
+    phone = models.IntegerField(help_text="Enter the phone number")
+    offer_price = models.IntegerField(help_text="Enter the offer price")
+    property_address = models.ManyToManyField(PropertyAddress, help_text="Enter the address of property")
+    payment = models.ManyToManyField(Payment, help_text="Enter the payment")
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.first_name
